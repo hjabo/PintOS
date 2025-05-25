d74bf3c9195435653ae29234b7f04c4a3a2cdd4a
hjabo <hjabo@naver.com>
2025-05-25 18:48:14 +0900

    pass 22

--- userprog/build/userprog/syscall.d
+++ userprog/build/userprog/syscall.d
@@ -3,6 +3,5 @@ userprog/syscall.o: ../../userprog/syscall.c ../../userprog/syscall.h \
  ../../lib/stdbool.h ../../lib/stddef.h ../../lib/stdint.h \
  ../../lib/kernel/stdio.h ../../lib/syscall-nr.h \
  ../../threads/interrupt.h ../../threads/thread.h ../../lib/kernel/list.h \
- ../../threads/synch.h ../../threads/init.h ../../threads/vaddr.h \
- ../../threads/loader.h ../../devices/shutdown.h ../../userprog/process.h \
- ../../filesys/filesys.h ../../filesys/off_t.h
+ ../../threads/synch.h ../../threads/vaddr.h ../../threads/loader.h \
+ ../../userprog/process.h ../../filesys/filesys.h ../../filesys/off_t.h
--- userprog/syscall.c
+++ userprog/syscall.c
@@ -3,13 +3,17 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
-#include "threads/init.h"
 #include "threads/vaddr.h"
-#include "devices/shutdown.h"
 #include "userprog/process.h"
 #include "filesys/filesys.h"
 
-static void syscall_handler(struct intr_frame *);
+void syscall_handler(struct intr_frame *);
+
+void
+syscall_init (void) 
+{
+  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
 
 void sys_halt(void);
 void sys_exit(int status);
@@ -17,36 +21,37 @@ int sys_exec(const char *file);
 int sys_wait(int pid);
 bool sys_create(const char *file, unsigned initial_size);
 bool sys_remove(const char *file);
-int sys_write(int fd, const void *buffer, unsigned size);
-void
-syscall_init (void) 
-{
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
-}
+static int sys_write(int fd, const void *buffer, unsigned size);
 
-static void syscall_handler(struct intr_frame *f UNUSED){
-  int syscall_number = *((int *)f->esp);
-  printf("[][] syscall_handler entered. syscall number = %d\n",syscall_number);
+
+void syscall_handler(struct intr_frame *f){
+  uint32_t syscall_number = *((uint32_t *)f->esp);
   switch(syscall_number){
     case SYS_HALT:
       sys_halt();
     case SYS_EXIT:
-      sys_exit(*((int *)f->esp+1));
-      break;
-    case SYS_EXEC:
-      f->eax = sys_exec(*((int *)f->esp+1));
-      break;
-    case SYS_WAIT:
-      f->eax = sys_wait(*((int *)f->esp+1));
-      break;
-    case SYS_CREATE:
-//      f->eax = sys_create((const char*)*((int *)f->esp+1), (unsigned)*((int *)f->esp+2));
-      break;
-    case SYS_REMOVE:
-//      f->eax = sys_remove((const char*)*((int *)f->esp+1));
+      sys_exit(*(int*)(f->esp+4));
       break;
     case SYS_WRITE:
-      sys_write((int)*((int *)f->esp+1), (void *)*((int *)f->esp+2), (unsigned)*((int *)f->esp+3));
+      {
+        int fd = *(int*)(f->esp+4);
+        void* buffer = (void *)*(int*)(f->esp+8);
+        unsigned size = *(unsigned*)(f->esp+12);
+        f->eax = sys_write(fd,buffer,size);
+      }
+      break;
+    case SYS_EXEC:
+      f->eax = sys_exec(*(int *)(f->esp+4));
+      break;
+    case SYS_WAIT:
+      f->eax = sys_wait(*(int *)(f->esp+4));
+      break;
+    case SYS_CREATE:
+      f->eax = sys_create((const char*)*(int *)(f->esp+4), (unsigned)*(int *)(f->esp+8));
+      break;
+    case SYS_REMOVE:
+      f->eax = sys_remove((const char*)*(int *)(f->esp+4));
+      break;
     default:
       sys_exit(-1);
   }
@@ -55,11 +60,12 @@ static void syscall_handler(struct intr_frame *f UNUSED){
 
 
 void sys_halt(void){
-  shutdown();
+  shutdown_power_off();
 }
 void sys_exit(int status){
-  struct thread *cur = thread_current();
-  printf("%s: exit(%d)\n",thread_name(),status);	
+  struct thread* cur = thread_current();	
+
+  printf("%s: exit(%d)\n",cur->name,status);
   cur->exit_status = status;
   thread_exit();
 }
@@ -76,11 +82,12 @@ bool sys_remove(const char *file){
   return filesys_remove(file);
 }
 
-int sys_write(int fd, const void *buffer, unsigned size){
-  if(fd==1){
-    putbuf(buffer,size);
-    return size;
-  }
-  return -1;
+static int sys_write(int fd, const void *buffer, unsigned size){
+  if(fd!=1&&fd!=2)
+    return -1;
+  if(!is_user_vaddr(buffer) || !is_user_vaddr(buffer+size-1))
+    sys_exit(-1);
+  putbuf(buffer,size);
+  return size;
 }
 
